4c8975d5538613160221c4ec5a5ff9148196cd7e
KangHoML <kanghokhkh1@gmail.com>
2024-02-12 13:21:55 +0900

    need to program

---
+++ 2024-02-02/data.py
@@ -0,0 +1,15 @@
+import os
+import torchvision.transforms as T
+
+from PIL import Image
+from torch.utils.data import Dataset
+
+class CIFAR10Dataset(Dataset):
+    def __init__(self):
+        return
+
+    def __len__(self):
+        return
+
+    def __getitem__(self, idx):
+        return 
---
+++ 2024-02-02/net.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class VGG(nn.module):
+    def __init__(self):
+        return
+    
+    def forward(self, x):
+        return
+    
+class ResNet(nn.Module):
+    def __init__(self):
+        return
+    
+    def forward(self, x):
+        return

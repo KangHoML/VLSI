18c668341152eaf0ead488ffb1515d11ecb7612b
KangHoML <kanghokhkh1@gmail.com>
2024-03-02 16:58:23 +0900

    20240302

---
+++ 2024-02-23/transformer.py
@@ -0,0 +1,195 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# input : (seq_len, emb_size)
+class MultiHeadAttention(nn.Module):
+    def __init__(self, hidden_size, num_heads=8):
+        super().__init__()
+
+        self.num_heads = num_heads
+        self.hidden_size = hidden_size
+        self.attn_size = hidden_size // num_heads
+
+        self.query_embed= nn.Linear(hidden_size, hidden_size)
+        self.key_embed= nn.Linear(hidden_size, hidden_size)
+        self.value_embed= nn.Linear(hidden_size, hidden_size)
+        self.output_embed= nn.Linear(hidden_size, hidden_size)
+
+    def scaled_dot_product_attention(self, query, key, value, mask=None):
+        d_k = key.size()[-1]
+        key_T = torch.transpose(key, 3, 2)
+
+        attn_score = torch.matmul(query, key_T) / math.sqrt(d_k)
+        
+        if mask is not None:
+            attn_score = attn_score.masked_fill(mask.unsqueeze(1).unsqueeze(-1), 0)
+
+        attn_weight = F.softmax(attn_score, -1)
+        attn_value = torch.matmul(attn_weight, value)
+
+        return attn_value
+    
+    def forward(self, query, key, value, mask=None):
+        batch_size = query.size()[0]
+
+        # (batch_size, seq_len, hidden_size) -> (batch_size, num_heads, attn_size, attn_size)
+        query = self.query_embed(query).view(batch_size, -1, self.num_heads, self.attn_size).transpose(1, 2)
+        key = self.key_embed(key).view(batch_size, -1, self.num_heads, self.attn_size).transpose(1, 2)
+        value = self.value_embed(value).view(batch_size, -1, self.num_heads, self.attn_size).transpose(1, 2)
+
+        attn_value = self.scaled_dot_product_attention(query, key, value, mask)
+        attn_value = torch.transpose(attn_value, 1, 2).contiguous().view((batch_size, -1, self.hidden_size))
+
+        return attn_value
+
+class FeedForword(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.layer1 = nn.Linear(hidden_size, hidden_size * 4)
+        self.act = nn.ReLU()
+        self.layer2 = nn.Linear(hidden_size * 4, hidden_size)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.act(x)
+        x = self.layer2(x)
+
+        return x
+
+class EncoderLayer(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        
+        self.attention = MultiHeadAttention(hidden_size)
+        self.norm = nn.LayerNorm(hidden_size)
+        self.ffnn = FeedForword(hidden_size)
+
+    def forward(self, src, mask):
+        attn_value = self.attention(src, src, src, mask)
+        res_out = src + self.norm(attn_value)
+        
+        ffnn_output = self.ffnn(res_out)
+        output = res_out + self.norm(ffnn_output)
+
+        return output
+
+class Encoder(nn.Module):
+    def __init__(self, hidden_size, num_layers):
+        super().__init__()
+        self.encoder = [EncoderLayer(hidden_size) for _ in range(num_layers)]
+
+    def forward(self, src, mask):
+        encoder_output = src
+        for encoder in self.encoder:
+            encoder_output = encoder(encoder_output, mask)
+        
+        return encoder_output
+
+class DecoderLayer(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+
+        self.attention = MultiHeadAttention(hidden_size)
+        self.norm = nn.LayerNorm(hidden_size)
+        self.ffnn = FeedForword(hidden_size)
+    
+    def forward(self, trg, encoder_output, mask):
+        look_ahead_attn = self.attention(trg, trg, trg, mask)
+        res_out = trg + self.norm(look_ahead_attn)
+        
+        attn = self.attention(encoder_output, encoder_output, res_out)
+        res_out = res_out + self.norm(attn)
+
+        ffnn_out = self.ffnn(res_out)
+        output = res_out + self.norm(ffnn_out)
+
+        return output
+    
+class Decoder(nn.Module):
+    def __init__(self, hidden_size, num_layers):
+        super().__init__()
+        self.decoder = [DecoderLayer(hidden_size) for _ in range(num_layers)]
+
+    def forward(self, trg, encoder_output, mask):
+        decoder_output = trg
+        for decoder in self.decoder:
+            decoder_output = decoder(decoder_output, encoder_output, mask)
+
+        return decoder_output
+    
+class Transformer(nn.Module):
+    def __init__(self, hidden_size, num_layers, src_vocab_size, trg_vocab_size,
+                 src_max_seq_len, trg_max_seq_len):
+        super().__init__()
+
+        self.hidden_size = hidden_size
+        self.attn_size = hidden_size // 2
+        self.src_vocab_size = src_vocab_size
+        self.trg_vocab_size = trg_vocab_size
+        self.src_max_seq_len = src_max_seq_len
+        self.trg_max_seq_len = trg_max_seq_len
+
+        self.src_embed = nn.Embedding(src_vocab_size, hidden_size)
+        self.encoder = Encoder(hidden_size, num_layers)
+
+        self.trg_embed = nn.Embedding(trg_vocab_size, hidden_size)
+        self.decoder= Decoder(hidden_size, num_layers)
+
+        self.output = nn.Sequential(
+            nn.Linear(hidden_size, trg_max_seq_len),
+            nn.Softmax(dim=-1)
+        )
+
+    def positional_encoding(self, max_seq_len):
+        position = torch.arange(0, max_seq_len, dtype=torch.float).unsqueeze(1)
+        pe = torch.zeros(max_seq_len, self.hidden_size)
+        div_term = torch.pow(torch.ones(self.attn_size).fill_(10000),
+                             torch.arange(0, self.hidden_size, 2) / torch.tensor(self.hidden_size, dtype=torch.float32))
+        
+        pe[:, 0::2] = torch.sin(position / div_term)
+        pe[:, 1::2] = torch.cos(position / div_term)
+
+        pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+        return pe
+
+    def forward(self, src, trg, trg_mask, src_mask=None):
+        src_embed = self.src_embed(src)
+        src_embed += self.positional_encoding(self.src_max_seq_len)
+        encoder_output =self.encoder(src_embed, src_mask)
+
+        trg_embed = self.trg_embed(trg)
+        trg_embed += self.positional_encoding(self.trg_max_seq_len)
+        decoder_output = self.decoder(trg_embed, encoder_output, trg_mask)
+
+        output = self.output(decoder_output)
+
+        return output
+
+if __name__ == "__main__":
+    src_vocab_size, trg_vocab_size = 4488, 7884
+    max_seq_len = 16
+    
+    net = Transformer(512, 6, src_vocab_size, trg_vocab_size, max_seq_len, max_seq_len)
+    
+    src = torch.randint(low=0, high=src_vocab_size, size=(16, max_seq_len), dtype=torch.long)
+    trg = torch.randint(low=0, high=trg_vocab_size, size=(16, max_seq_len), dtype=torch.long)
+    
+    mask = torch.zeros((16, max_seq_len)).to(torch.bool)
+    mask[:, :30] = 1
+
+    output = net(src, trg, trg_mask=mask)
+    print(output.shape)
+
+
+
+
+
+
+
+
+
+

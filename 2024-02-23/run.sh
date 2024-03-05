0acfadd0314dd26d5b2e648fd604c7e4be77c614
KangHoML <kanghokhkh1@gmail.com>
2024-03-05 17:07:42 +0900

    2024-03-05

--- 2024-02-23/run.sh
+++ 2024-02-23/run.sh
@@ -1,11 +1,11 @@
-# python3 train.py --data_path "../../datasets/fra_eng.txt" \
-#                  --model "Seq2Seq" \
-#                  --hidden_size 256 \
-#                  > ./result/Seq2Seq.txt
-
 python3 train.py --data_path "../../datasets/fra_eng.txt" \
-                 --model "Transformer" \
-                 --hidden_size 512 \
-                 --num_layer 6 \
-                 --max_seq_len 16 \
-                 > ./result/Transformer.txt
+                 --model "Seq2Seq" \
+                 --hidden_size 256 \
+                 > ./result/Seq2Seq.txt
+
+# python3 train.py --data_path "../../datasets/fra_eng.txt" \
+#                  --model "Transformer" \
+#                  --hidden_size 512 \
+#                  --num_layer 6 \
+#                  --max_seq_len 16 \
+#                  > ./result/Transformer.txt
---
+++ 2024-02-23/translate.py
@@ -0,0 +1,6 @@
+
+def translate():
+    raise NotImplemented
+
+if __name__ == "__main__":
+    translate()

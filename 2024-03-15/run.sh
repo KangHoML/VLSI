26fdd89e7c8b11c05e15e2df3385ea4345eaa654
KangHoML <kanghokhkh1@gmail.com>
2024-03-20 23:32:43 +0900

    2024-03-20 train

--- 2024-03-15/run.sh
+++ 2024-03-15/run.sh
@@ -1,11 +1,11 @@
 torchrun --nproc_per_node=4 train.py --data_path ../datasets/CIFAR-10 \
                                      --model "ResNeXt50" \
-                                     > ./result/ResNext.txt
+                                     > ./result/ResNeXt.txt
 
 torchrun --nproc_per_node=4 train.py --data_path ../datasets/CIFAR-10 \
                                      --model "ResNeXt101" \
-                                     >> ./result/ResNext.txt
+                                     >> ./result/ResNeXt.txt
 
 torchrun --nproc_per_node=4 train.py --data_path ../datasets/CIFAR-10 \
                                      --model "ResNeXt152" \
-                                     >> ./result/ResNext.txt
+                                     >> ./result/ResNeXt.txt

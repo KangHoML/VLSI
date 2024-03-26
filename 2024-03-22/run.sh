dd6e1072c26e78116369c9f9d15457708e3f44ce
KangHoML <kanghokhkh1@gmail.com>
2024-03-26 15:24:40 +0000

    pin_memory?

--- 2024-03-22/run.sh
+++ 2024-03-22/run.sh
@@ -3,6 +3,17 @@ torchrun --nproc_per_node 4 train.py --is_ddp True \
                                      --hidden_size 256 \
                                      --embed_size 128 \
                                      --dropout 0.2 \
+				     --step_size 1.0 \
+				     --gamma 1.0 \
                                      --batch_size 64 \
                                      --learning_rate 1e-3 \
                                      --epoch 10
+
+#python3 train.py --is_amp False \
+#                 --hidden_size 256 \
+#                 --embed_size 128 \
+#                 --dropout 0.2 \
+#                 --batch_size 64 \
+#                 --learning_rate 1e-3 \
+#                 --epoch 10
+

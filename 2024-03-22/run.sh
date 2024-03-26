adfa39e4e7a295fbfa4c675686f54e96e98a9171
KangHoML <kanghokhkh1@gmail.com>
2024-03-26 17:38:57 +0000

    gru ok

--- 2024-03-22/run.sh
+++ 2024-03-22/run.sh
@@ -4,10 +4,13 @@ torchrun --nproc_per_node 4 train.py --is_ddp True \
                                      --embed_size 128 \
                                      --dropout 0.2 \
                                      --model 'gru' \
-                                     --gamma 1.0 \
+				     --n_layers 2 \
+				     --step_size 5 \
+                                     --gamma 0.1 \
                                      --batch_size 64 \
-                                     --learning_rate 1e-3 \
-                                     --epoch 10
+                                     --learning_rate 1e-2 \
+                                     --epoch 10 \
+				     > ./result/deep_gru.txt
 
 # python3 train.py --is_amp False \
 #                 --hidden_size 256 \

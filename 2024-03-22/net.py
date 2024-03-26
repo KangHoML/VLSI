3929b3885b24f44e77fe024e90c54baf6948e32e
KangHoML <kanghokhkh1@gmail.com>
2024-03-27 00:04:59 +0900

    initalize network

--- 2024-03-22/net.py
+++ 2024-03-22/net.py
@@ -11,7 +11,10 @@ class SentenceClassifier(nn.Module):
     def __init__(self, n_vocab, hidden_size, embed_size, n_layers=1,
                  dropout=0, bidirectional=False, model_type='lstm'):
         super().__init__()
-    
+        
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        
         self.embedding = nn.Embedding(n_vocab, embed_size, padding_idx=0)
 
         if model_type == "rnn":
@@ -33,14 +36,19 @@ class SentenceClassifier(nn.Module):
 
     def forward(self, x):
         embedded = self.embedding(x)
+        h_0 = self._init_state(batch_size=embedded.size(0))
 
-        output, _ = self.context(embedded)
+        output, _ = self.context(embedded, h_0)
         output = output[:, -1, :] # last time_step of output
         
         output = self.dropout(output)
         logits = self.classifier(output)
         
         return logits
+    
+    def _init_state(self, batch_size=1):
+        weight = next(self.parameters()).data
+        return weight.new(self.n_layers, batch_size, self.hidden_size).zero_()
 
 if __name__ == "__main__":
     src_vocab_size = 40710

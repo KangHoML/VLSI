81d9f90ad862c90a0c33f71073d240e870b49219
KangHoML <kanghokhkh1@gmail.com>
2024-03-26 15:39:03 +0000

    idk

--- 2024-03-22/run.sh
+++ 2024-03-22/run.sh
@@ -3,8 +3,9 @@ torchrun --nproc_per_node 4 train.py --is_ddp True \
                                      --hidden_size 256 \
                                      --embed_size 128 \
                                      --dropout 0.2 \
-				                     --step_size 1.0 \
-				                     --gamma 0.1 \
+                                     --model 'gru' \
+                                     --step_size 1.0 \
+                                     --gamma 1.0 \
                                      --batch_size 64 \
                                      --learning_rate 1e-3 \
                                      --epoch 10

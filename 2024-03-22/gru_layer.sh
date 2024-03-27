0ee6ce893c241653a89fb21f1b77dbfbccf653a1
KangHoML <kanghokhkh1@gmail.com>
2024-03-27 19:43:14 +0900

    need to train

--- 2024-03-22/gru_layer.sh
+++ 2024-03-22/gru_layer.sh
@@ -2,10 +2,9 @@
 torchrun --nproc_per_node 4 train.py --is_ddp True \
                                      --is_amp False \
                                      --model 'gru' \
-                                     --hidden_size 256 \
+                                     --hidden_size 512 \
                                      --embed_size 128 \
                                      --n_layers 1 \
-                                     --dropout 0.2 \
                                      --step_size 5 \
                                      --gamma 0.1 \
                                      --batch_size 64 \

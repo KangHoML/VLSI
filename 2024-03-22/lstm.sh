293540982cd5cc4c4c8fd231a371acf667c491ec
KangHoML <kanghokhkh1@gmail.com>
2024-03-27 18:30:28 +0000

    need to test

--- 2024-03-22/lstm.sh
+++ 2024-03-22/lstm.sh
@@ -1,25 +1,25 @@
-torchrun --nproc_per_node 4 train.py --is_ddp True \
-                                     --is_amp False \
-                                     --model 'lstm' \
-                                     --hidden_size 256 \
-                                     --embed_size 256 \
-                                     --n_layers 1 \
-                                     --step_size 5 \
-                                     --gamma 0.1 \
-                                     --batch_size 64 \
-                                     --learning_rate 1e-2 \
-                                     --epoch 10 \
-                                     --save "lstm_ddp" \
-                                     > ./result/lstm.txt
+# torchrun --nproc_per_node 4 train.py --is_ddp True \
+#                                      --is_amp False \
+#                                      --model 'lstm' \
+#                                      --hidden_size 512 \
+#                                      --embed_size 256 \
+#                                      --n_layers 1 \
+#                                      --step_size 5 \
+#                                      --gamma 1.0 \
+#                                      --batch_size 32 \
+#                                      --learning_rate 1e-2 \
+#                                      --epoch 10 \
+#                                      --save "lstm_ddp" \
+#                                      > ./result/lstm.txt
 
 python3 train.py --model 'lstm' \
-                 --hidden_size 256 \
+                 --hidden_size 512 \
                  --embed_size 256 \
                  --n_layers 1 \
-                 --step_size 5 \
+                 --step_size 7 \
                  --gamma 0.1 \
                  --batch_size 64 \
                  --learning_rate 1e-2 \
                  --epoch 10 \
                  --save "lstm" \
-                 > ./result/lstm.txt
+		 >> ./result/lstm.txt

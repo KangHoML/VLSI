84d8cd334cce1355fde7ee12b392c086b22685df
KangHoML <kanghokhkh1@gmail.com>
2024-04-04 15:06:50 +0900

    modify the model

--- 2024-03-29/run.sh
+++ 2024-03-29/run.sh
@@ -1,19 +1,25 @@
 python3 train.py --tokenizer 'okt' \
                  --vocab_size 20000 \
                  --max_len 32 \
+                 --learning_rate 2e-5 \
+                 --save 'okt' \
                  > './result/okt.txt'
 
 python3 test.py --tokenizer 'okt' \
                  --vocab_size 20000 \
                  --max_len 32 \
+                 --pth_name 'okt' \
                  >> './result/okt.txt'
 
 python3 train.py --tokenizer 'spm' \
                  --vocab_size 20000 \
                  --max_len 32 \
+                 --learning_rate 2e-5 \
+                 --save 'spm' \
                  > './result/spm.txt'
 
 python3 test.py --tokenizer 'spm' \
                  --vocab_size 20000 \
                  --max_len 32 \
+                 --pth_name 'spm' \
                  >> './result/spm.txt'

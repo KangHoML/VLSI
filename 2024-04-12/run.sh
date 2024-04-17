9766f6ea4e8bcef5d817ed148e4134b46b049b4e
KangHoML <kanghokhkh1@gmail.com>
2024-04-18 00:53:46 +0900

    convnext

--- 2024-04-12/run.sh
+++ 2024-04-12/run.sh
@@ -1,6 +1,9 @@
 python3 main.py --pretrained False \
                 --optimizer 'SGD' \
-                --batch_size 16 \
-                --learning_rate 4e-4 \
-                --epoch 10 \
-                --save 'convnext'
+                --batch_size 32 \
+                --learning_rate 1e-2 \
+                --lr_scheduler 'Cosine' \
+                --step_size 50 \
+                --epoch 100 \
+                --save 'convnext' \
+                > './result/convnext.txt'

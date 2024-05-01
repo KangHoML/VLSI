d87ff8c9ebe43d15ec5e8e31471c860e806b5a54
KangHoML <kanghokhkh1@gmail.com>
2024-05-02 00:27:42 +0900

    hirerichical

--- 2024-04-26/run.sh
+++ 2024-04-26/run.sh
@@ -1,16 +1,16 @@
 # ResNet Auto Encoder Training
-# python3 train.py --batch_size 16 \
-#                  --learning_rate 1e-4 \
-#                  --epoch 20 \
-#                  --save "resnet_ae" \
-#                  > "./result/loss.txt"
+python3 train.py --batch_size 16 \
+                 --learning_rate 1e-4 \
+                 --epoch 20 \
+                 --save "resnet_ae" \
+                 > "./result/loss.txt"
 
-# python3 plot.py --sample 5 \
-#                 --load "resnet_ae"
+python3 plot.py --sample 5 \
+                --load "resnet_ae"
 
 # Clustering
-# python3 cluster.py --cluster "KMeans" \
-#                    --load "resnet_ae"
+python3 cluster.py --cluster "KMeans" \
+                   --load "resnet_ae"
 
 python3 cluster.py --cluster "Hierarchical" \
                    --load "resnet_ae"

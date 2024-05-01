bc0bd059a28e4f4bda011f455b813b0674bf3766
KangHoML <kanghokhkh1@gmail.com>
2024-05-01 21:30:46 +0900

    modify colormap

--- 2024-04-26/run.sh
+++ 2024-04-26/run.sh
@@ -1,12 +1,12 @@
 # ResNet Auto Encoder Training
-python3 train.py --batch_size 16 \
-                 --learning_rate 1e-4 \
-                 --epoch 20 \
-                 --save "resnet_ae" \
-                 > "./result/loss.txt"
+# python3 train.py --batch_size 16 \
+#                  --learning_rate 1e-4 \
+#                  --epoch 20 \
+#                  --save "resnet_ae" \
+#                  > "./result/loss.txt"
 
-python3 test.py --sample 5 \
-                --load "resnet_ae"
+# python3 test.py --sample 5 \
+#                 --load "resnet_ae"
 
 # Clustering
 python3 cluster.py --cluster "KMeans" \

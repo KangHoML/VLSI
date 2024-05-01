a2ae79b78b1abe389dc4035896b38a38f85e19b0
KangHoML <kanghokhkh1@gmail.com>
2024-05-01 20:11:04 +0900

    train finish

--- 2024-04-26/run.sh
+++ 2024-04-26/run.sh
@@ -1,9 +1,10 @@
 python3 train.py --batch_size 16 \
                  --learning_rate 1e-4 \
-                 --epoch 30 \
-                 --save ckpt1 \
+                 --epoch 20 \
+                 --save resnet_ae \
+                 > ./result/loss.txt
 
 python3 test.py --batch_size 16 \
                 --sample 5 \
-                --load ckpt1
+                --load resnet_ae
                 

57a094e3825cb6d46e5f06015712290763b83a6e
KangHoML <kanghokhkh1@gmail.com>
2024-05-01 21:02:38 +0900

    because of batch

--- 2024-04-26/run.sh
+++ 2024-04-26/run.sh
@@ -2,11 +2,11 @@
 python3 train.py --batch_size 16 \
                  --learning_rate 1e-4 \
                  --epoch 20 \
-                 --save resnet_ae \
-                 > ./result/loss.txt
+                 --save "resnet_ae" \
+                 > "./result/loss.txt"
 
 python3 test.py --sample 5 \
-                --load resnet_ae
+                --load "resnet_ae"
 
 python3 cluster.py --cluster "KMeans" \
-                   --load resnet_ae
+                   --load "resnet_ae"
